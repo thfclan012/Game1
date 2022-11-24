be2259bb070cee19405f58b3ce311d237afedfa5
ΝΑΟΥΜ ΓΑΛΑΤΑΣ <na54.galatas@iek-alfa.gr>
2022-11-24 17:29:42 +0200

    time

--- Game1/Form1.Designer.cs
+++ Game1/Form1.Designer.cs
@@ -44,21 +44,25 @@ namespace Game1
       this.button1 = new System.Windows.Forms.Button();
       this.button2 = new System.Windows.Forms.Button();
       this.button3 = new System.Windows.Forms.Button();
+      this.panel1 = new System.Windows.Forms.Panel();
+      this.label7 = new System.Windows.Forms.Label();
+      this.panel1.SuspendLayout();
       this.SuspendLayout();
       // 
       // label1
       // 
       this.label1.AutoSize = true;
       this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-      this.label1.Location = new System.Drawing.Point(26, 100);
+      this.label1.Location = new System.Drawing.Point(26, 22);
       this.label1.Name = "label1";
-      this.label1.Size = new System.Drawing.Size(37, 15);
+      this.label1.Size = new System.Drawing.Size(49, 15);
       this.label1.TabIndex = 0;
-      this.label1.Text = "label1";
+      this.label1.Text = "Strenght";
       // 
       // textBox1
       // 
-      this.textBox1.Location = new System.Drawing.Point(87, 97);
+      this.textBox1.Enabled = false;
+      this.textBox1.Location = new System.Drawing.Point(104, 17);
       this.textBox1.Name = "textBox1";
       this.textBox1.Size = new System.Drawing.Size(100, 20);
       this.textBox1.TabIndex = 1;
@@ -67,122 +71,151 @@ namespace Game1
       // 
       this.label2.AutoSize = true;
       this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-      this.label2.Location = new System.Drawing.Point(26, 128);
+      this.label2.Location = new System.Drawing.Point(26, 57);
       this.label2.Name = "label2";
-      this.label2.Size = new System.Drawing.Size(37, 15);
+      this.label2.Size = new System.Drawing.Size(50, 15);
       this.label2.TabIndex = 2;
-      this.label2.Text = "label2";
+      this.label2.Text = "Dexterity";
       // 
       // label3
       // 
       this.label3.AutoSize = true;
       this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-      this.label3.Location = new System.Drawing.Point(26, 160);
+      this.label3.Location = new System.Drawing.Point(26, 92);
       this.label3.Name = "label3";
-      this.label3.Size = new System.Drawing.Size(37, 15);
+      this.label3.Size = new System.Drawing.Size(61, 15);
       this.label3.TabIndex = 3;
-      this.label3.Text = "label3";
+      this.label3.Text = "Prostitution";
       // 
       // label4
       // 
       this.label4.AutoSize = true;
       this.label4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-      this.label4.Location = new System.Drawing.Point(26, 190);
+      this.label4.Location = new System.Drawing.Point(24, 122);
       this.label4.Name = "label4";
-      this.label4.Size = new System.Drawing.Size(37, 15);
+      this.label4.Size = new System.Drawing.Size(63, 15);
       this.label4.TabIndex = 4;
-      this.label4.Text = "label4";
+      this.label4.Text = "Intelligence";
       // 
       // label6
       // 
       this.label6.AutoSize = true;
       this.label6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-      this.label6.Location = new System.Drawing.Point(26, 257);
+      this.label6.Location = new System.Drawing.Point(26, 191);
       this.label6.Name = "label6";
-      this.label6.Size = new System.Drawing.Size(37, 15);
+      this.label6.Size = new System.Drawing.Size(52, 15);
       this.label6.TabIndex = 5;
-      this.label6.Text = "label6";
+      this.label6.Text = "Charisma";
       // 
       // label5
       // 
       this.label5.AutoSize = true;
       this.label5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-      this.label5.Location = new System.Drawing.Point(26, 222);
+      this.label5.Location = new System.Drawing.Point(26, 156);
       this.label5.Name = "label5";
-      this.label5.Size = new System.Drawing.Size(37, 15);
+      this.label5.Size = new System.Drawing.Size(47, 15);
       this.label5.TabIndex = 6;
-      this.label5.Text = "label5";
+      this.label5.Text = "Wisdom";
       // 
       // textBox2
       // 
-      this.textBox2.Location = new System.Drawing.Point(87, 125);
+      this.textBox2.Enabled = false;
+      this.textBox2.Location = new System.Drawing.Point(104, 52);
       this.textBox2.Name = "textBox2";
       this.textBox2.Size = new System.Drawing.Size(100, 20);
       this.textBox2.TabIndex = 7;
       // 
       // textBox3
       // 
-      this.textBox3.Location = new System.Drawing.Point(87, 155);
+      this.textBox3.Enabled = false;
+      this.textBox3.Location = new System.Drawing.Point(104, 87);
       this.textBox3.Name = "textBox3";
       this.textBox3.Size = new System.Drawing.Size(100, 20);
       this.textBox3.TabIndex = 8;
       // 
       // textBox4
       // 
-      this.textBox4.Location = new System.Drawing.Point(87, 185);
+      this.textBox4.Enabled = false;
+      this.textBox4.Location = new System.Drawing.Point(104, 117);
       this.textBox4.Name = "textBox4";
       this.textBox4.Size = new System.Drawing.Size(100, 20);
       this.textBox4.TabIndex = 9;
       // 
       // textBox5
       // 
-      this.textBox5.Location = new System.Drawing.Point(87, 219);
+      this.textBox5.Enabled = false;
+      this.textBox5.Location = new System.Drawing.Point(104, 151);
       this.textBox5.Name = "textBox5";
       this.textBox5.Size = new System.Drawing.Size(100, 20);
       this.textBox5.TabIndex = 10;
       // 
       // textBox6
       // 
-      this.textBox6.Location = new System.Drawing.Point(87, 252);
+      this.textBox6.Enabled = false;
+      this.textBox6.Location = new System.Drawing.Point(104, 186);
       this.textBox6.Name = "textBox6";
       this.textBox6.Size = new System.Drawing.Size(100, 20);
       this.textBox6.TabIndex = 11;
       // 
       // button1
       // 
-      this.button1.Location = new System.Drawing.Point(12, 303);
+      this.button1.Location = new System.Drawing.Point(5, 14);
       this.button1.Name = "button1";
       this.button1.Size = new System.Drawing.Size(75, 23);
       this.button1.TabIndex = 12;
-      this.button1.Text = "ROLE";
+      this.button1.Text = "ROLL";
       this.button1.UseVisualStyleBackColor = true;
+      this.button1.Click += new System.EventHandler(this.button1_Click);
       // 
       // button2
       // 
-      this.button2.Location = new System.Drawing.Point(93, 303);
+      this.button2.Location = new System.Drawing.Point(86, 14);
       this.button2.Name = "button2";
       this.button2.Size = new System.Drawing.Size(75, 23);
       this.button2.TabIndex = 13;
       this.button2.Text = "RESET";
       this.button2.UseVisualStyleBackColor = true;
+      this.button2.Click += new System.EventHandler(this.button2_Click);
       // 
       // button3
       // 
-      this.button3.Location = new System.Drawing.Point(174, 303);
+      this.button3.Location = new System.Drawing.Point(167, 14);
       this.button3.Name = "button3";
       this.button3.Size = new System.Drawing.Size(75, 23);
       this.button3.TabIndex = 14;
       this.button3.Text = "CONFIRM";
       this.button3.UseVisualStyleBackColor = true;
       // 
+      // panel1
+      // 
+      this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+      this.panel1.Controls.Add(this.label7);
+      this.panel1.Controls.Add(this.button3);
+      this.panel1.Controls.Add(this.button1);
+      this.panel1.Controls.Add(this.button2);
+      this.panel1.Location = new System.Drawing.Point(24, 228);
+      this.panel1.Name = "panel1";
+      this.panel1.Size = new System.Drawing.Size(245, 64);
+      this.panel1.TabIndex = 15;
+      // 
+      // label7
+      // 
+      this.label7.AutoSize = true;
+      this.label7.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+      this.label7.Location = new System.Drawing.Point(17, 40);
+      this.label7.Name = "label7";
+      this.label7.Size = new System.Drawing.Size(37, 15);
+      this.label7.TabIndex = 16;
+      this.label7.Text = "label7";
+      this.label7.Visible = false;
+      this.label7.Click += new System.EventHandler(this.label7_Click);
+      // 
       // Form1
       // 
       this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-      this.ClientSize = new System.Drawing.Size(800, 450);
-      this.Controls.Add(this.button3);
-      this.Controls.Add(this.button2);
-      this.Controls.Add(this.button1);
+      this.ClientSize = new System.Drawing.Size(574, 304);
+      this.Controls.Add(this.panel1);
       this.Controls.Add(this.textBox6);
       this.Controls.Add(this.textBox5);
       this.Controls.Add(this.textBox4);
@@ -195,9 +228,12 @@ namespace Game1
       this.Controls.Add(this.label2);
       this.Controls.Add(this.textBox1);
       this.Controls.Add(this.label1);
+      this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
       this.Name = "Form1";
       this.Text = "Form1";
       this.Load += new System.EventHandler(this.Form1_Load);
+      this.panel1.ResumeLayout(false);
+      this.panel1.PerformLayout();
       this.ResumeLayout(false);
       this.PerformLayout();
 
@@ -220,6 +256,8 @@ namespace Game1
     private System.Windows.Forms.Button button1;
     private System.Windows.Forms.Button button2;
     private System.Windows.Forms.Button button3;
+    private System.Windows.Forms.Panel panel1;
+    private System.Windows.Forms.Label label7;
   }
 }
 
--- Game1/Form1.cs
+++ Game1/Form1.cs
@@ -12,6 +12,9 @@ namespace Game1
 {
   public partial class Form1 : Form
   {
+    //int index pou eimaste analogos to button press
+    int index = 1;
+
     public Form1()
     {
       InitializeComponent();
@@ -19,6 +22,88 @@ namespace Game1
 
     private void Form1_Load(object sender, EventArgs e)
     {
+
+    }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+      //ROLE
+      //if index= kati
+      switch (index)
+      {
+        case 1:
+          textBox1.Text = Roll().ToString();
+          break;
+        case 2:
+          textBox2.Text = Roll().ToString();
+          break;
+        case 3:
+          textBox3.Text = Roll().ToString();
+          break;
+        case 4:
+          textBox4.Text = Roll().ToString();
+          break;
+        case 5:
+          textBox5.Text = Roll().ToString();
+          break;
+        case 6:
+          textBox6.Text = Roll().ToString();
+          break;
+      }
+
+      //kate fora pou trexei index+1
+      index++;
+
+    }
+    //ROLL random numbers 3x times 
+    int Roll()
+    {
+      //set ints
+      int sum = 0;
+      int dice = 0;
+      Random rand = new Random();//create random
+
+      //set label7 visible 
+      label7.Text = "";
+      if (label7.Visible == false)
+      {
+        label7.Visible = true;
+      }
+      //run 3x times
+      for (int i=0; i < 3; i++)
+      {
+        dice = rand.Next(1, 6);
+        //removelast '+' from lablel7
+        if (i == 2)
+        {
+          label7.Text += dice;
+        }
+        else
+        {
+          label7.Text += string.Format("{0} + ", dice);
+        }
+        sum += dice;
+      }
+      return sum;
+    }
+
+    private void label7_Click(object sender, EventArgs e)
+    {
+
+    }
+
+    private void button2_Click(object sender, EventArgs e)
+    {
+      //reset button
+      index = 1;
+      textBox1.Text = "";
+      textBox2.Text = "";
+      textBox3.Text = "";
+      textBox4.Text = "";
+      textBox5.Text = "";
+      textBox6.Text = "";
+      label7.Text = "";
+      label7.Visible = false;
 
     }
   }
